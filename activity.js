be767714a0db28fc79a8590e9d2e133016db2138
dnikolos <dnikolos@gmail.com>
2019-07-25 09:15:17 +0300

    hit the walls

--- activity.js
+++ activity.js
@@ -1,5 +1,5 @@
 /*
-Copyright (C) 2018 Alkis Georgopoulos <alkisg@gmail.com>.
+Copyright (C) 2019 Dimitris Nikolos <dnikolos@gmail.com>.
 SPDX-License-Identifier: CC-BY-SA-4.0*/
 
 function onError(message, source, lineno, colno, error) {
@@ -160,32 +160,206 @@ function setOrientation(){
     case LT: ge('eprobot').style.transform = 'rotate(270deg)'; break;
   }
 }
+
+
+function animationNo(curPos,dir,hor){
+  /*animation with set interval 
+    curPos is in ems
+    ladybug moves in dir and returns back in curPos
+    when hor = true marginLeft
+    when hor = false marginTop
+    when dir = true right/down
+    when dir = false left/up
+  */
+  var endPos;
+  if (dir){
+    endPos = curPos + 3; //grid is 6em 3em is half block of grid
+  }
+  else{
+    endPos = curPos - 3;
+  }
+  var startPos = curPos;
+  var diff = (endPos - startPos)/5;
+  let i=0; 
+  inter1 = setInterval(function(){
+      console.log(startPos.toString()+endPos.toString())
+      if (Math.abs((startPos + i*diff - endPos)) < 0.01){
+        if (hor){
+          ge('eprobot').style.marginLeft = sformat("{}em",endPos);
+        }
+        else{
+          ge('eprobot').style.marginTop = sformat("{}em",endPos);
+        }
+        clearInterval(inter1);
+      }
+      else{
+        if (hor){
+          ge('eprobot').style.marginLeft = sformat("{}em",startPos + i*diff);
+        }
+        else{
+         ge('eprobot').style.marginTop = sformat("{}em",startPos + i*diff); 
+        }
+        i++;
+    }
+  },100);
+  setTimeout(function(){
+  //go back
+  startPos = endPos;
+  endPos = curPos;
+  diff = (endPos - startPos) / 5;
+  inter2 = setInterval(function(){
+      console.log(startPos.toString()+endPos.toString())
+      if (Math.abs((startPos + i*diff - endPos)) < 0.01){
+        if (hor){
+          ge('eprobot').style.marginLeft = sformat("{}em",endPos);
+        }
+        else{
+          ge('eprobot').style.marginTop = sformat("{}em",endPos);
+        }
+        clearInterval(inter2);
+        if (act.cmdExec < act.program.length){
+          act.cmdExec += 1
+          setTimeout(nextCommand,100);
+        }
+        else{
+          highlightCommand(-1);
+        }
+      }
+      else{
+        if (hor){
+          ge('eprobot').style.marginLeft = sformat("{}em",startPos + i*diff);
+        }
+        else{
+         ge('eprobot').style.marginTop = sformat("{}em",startPos + i*diff); 
+        }
+        i++;
+    }
+  },100);
+},500);
+
+
+}
+
+function animationSi(startPos,endPos,hor){
+  /*animation with set interval 
+    startpos is in ems
+    endpos is in ems
+    when hor = true marginLeft
+    when hor = false marginTop
+  */
+
+  var diff = (endPos - startPos)/10;
+  let i=0; 
+  inter = setInterval(function(){
+      console.log(startPos.toString()+endPos.toString())
+      if (Math.abs((startPos + i*diff - endPos)) < 0.01){
+        if (hor){
+          ge('eprobot').style.marginLeft = sformat("{}em",endPos);
+        }
+        else{
+          ge('eprobot').style.marginTop = sformat("{}em",endPos);
+        }
+        clearInterval(inter);
+        if (act.cmdExec < act.program.length){
+          act.cmdExec += 1
+          setTimeout(nextCommand,100);
+        }
+        else{
+          highlightCommand(-1);
+        }
+      }
+      else{
+        if (hor){
+          ge('eprobot').style.marginLeft = sformat("{}em",startPos + i*diff);
+        }
+        else{
+         ge('eprobot').style.marginTop = sformat("{}em",startPos + i*diff); 
+        }
+        i++;
+    }
+  },100);
+}
+function animationAn(startAngle,endAngle,clock){
+  /*angle animation startAngle and endAngle are in FD,LT,RT,BK format*/
+  var startAngleDeg,endAngleDeg;
+  switch (startAngle){
+    case FD: startAngleDeg = 0; break;
+    case RT: startAngleDeg = 90; break;
+    case BK: startAngleDeg = 180; break;
+    case LT: startAngleDeg = 270; break;
+  }
+  switch (endAngle){
+    case FD: endAngleDeg = 0; break;
+    case RT: endAngleDeg = 90; break;
+    case BK: endAngleDeg = 180; break;
+    case LT: endAngleDeg = 270; break;
+  }
+  var diff;
+  if (clock){
+    diff = 9; // 90 / 10 is 9 degrees for 1/10 of a second
+  }
+  else{
+    diff = -9;
+  }
+
+  let i=0; 
+  inter = setInterval(function(){
+      newAngle = startAngleDeg + i*diff;
+      console.log(startAngleDeg,i,diff,newAngle,endAngleDeg,startAngleDeg + i*diff - endAngleDeg);
+      if (Math.abs((360 + startAngleDeg + i*diff)%360 - endAngleDeg) < 0.01){
+        ge('eprobot').style.transform = sformat('rotate({}deg)',endAngleDeg);
+        clearInterval(inter);
+        if (act.cmdExec < act.program.length){
+          act.cmdExec += 1
+          setTimeout(nextCommand,100);
+        }
+        else{
+          highlightCommand(-1);
+        }
+      }
+      else{
+        ge('eprobot').style.transform = sformat('rotate({}deg)',startAngleDeg + i*diff);
+        i++;
+    }
+  },100);
+}
+
 function moveUp(){
   if (act.position[1] > 0){
-    act.position[1]--;
-    ge('eprobot').style.marginTop = sformat('{}em',act.position[1]*6);
+    animationSi(act.position[1]*6,(--act.position[1])*6,false);
+  }
+  else{
+    animationNo(act.position[1]*6,false,false);
   }
 }
 function moveDown(){
   if (act.position[1] < 4){
-    act.position[1]++;
-    ge('eprobot').style.marginTop = sformat('{}em',act.position[1]*6);
+    animationSi(act.position[1]*6,(++act.position[1])*6,false);
+  }
+  else{
+    animationNo(act.position[1]*6,true,false);
   }
 }
 function moveRight(){
   if (act.position[0] < 4){
-    act.position[0]++;
-    ge('eprobot').style.marginLeft = sformat('{}em',act.position[0]*6);
-  }  
+    animationSi(act.position[0]*6,(++act.position[0])*6,true);
+  }
+  else{
+    animationNo(act.position[0]*6,true,true);
+  }
 }
 function moveLeft(){
  if (act.position[0] > 0){
-    act.position[0]--;
-    ge('eprobot').style.marginLeft = sformat('{}em',act.position[0]*6);
+    animationSi(act.position[0]*6,(--act.position[0])*6,true);
   } 
+  else{
+    animationNo(act.position[0]*6,false,true);
+  }
 }
 
-function animation(cmdCode){
+function nextCommand(){
+  cmdCode = act.program[act.cmdExec];
+  highlightCommand(act.cmdExec);
   switch (cmdCode){
     case FD:
       switch (act.orientation){
@@ -204,12 +378,16 @@ function animation(cmdCode){
       }    
     break;
     case RT:
+      var startAngle = act.orientation;
       act.orientation = (act.orientation + 1) % 4;
-      setOrientation();
+      var endAngle = act.orientation;
+      animationAn(startAngle,endAngle,true);
     break;
     case LT:
+      var startAngle = act.orientation;
       act.orientation = (act.orientation + 3) % 4;
-      setOrientation();
+      var endAngle = act.orientation;
+      animationAn(startAngle,endAngle,false);
     break;
   }
 }
@@ -219,8 +397,9 @@ function restart(){
   act = {
     numOfCommands: 0,
     program: [],
-    position: [0,0],
+    position: [0,4],
     orientation: FD,
+    cmdExec: 0,
     play: false,//play means that the program is executed but may be it is paused
     pause: false,
   }
@@ -255,22 +434,16 @@ function init(){
   bindCommand('cright',RT);
 
   ge('cgo').addEventListener('click',function(event){
-    act.position = [0,0];
-    act.orientation = FD
+    act.position = [0,4];
+    act.orientation = FD;
     act.play = true;
     setOrientation();
     setSquare();
-    for (let i=0; i<act.program.length; i++){
-      setTimeout(function(){
-        if (!act.pause){
-          highlightCommand(i);
-          animation(act.program[i])
-        }
-      }, i*1000);
-    }
+    act.cmdExec = 0;
+    setTimeout(nextCommand,100);
     setTimeout(function(){
       highlightCommand(-1);//highlight none
-    },act.program.length*1000);
+    },(act.program.length+1)*1200);
   });
   
   ge('cdelete').addEventListener('click',restart);
